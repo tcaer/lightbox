faa384addd17ae9409f99467e0c545a4ae82cc87
tcaer <tino.caer@me.com>
2023-03-08 12:20:59 -0600

    Basic makefile for lbx

---
+++ lbx/include/lbx/lbx.h
@@ -0,0 +1,10 @@
+#ifndef LBX_H
+#define LBX_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void test_func();
+
+#endif
+
---
+++ lbx/src/test.c
@@ -0,0 +1,6 @@
+#include "lbx/lbx.h"
+
+void test_func() {
+  printf("test_func()\n");
+}
+
