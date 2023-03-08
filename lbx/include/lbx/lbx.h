38f3ba7af159ee1af8f0524688ef355e1d388ffe
tcaer <tino.caer@me.com>
2023-03-08 17:09:52 -0600

    Add basic type definitions

--- lbx/include/lbx/lbx.h
+++ lbx/include/lbx/lbx.h
@@ -4,6 +4,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Type definitions */
+// Unsigned ints
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+// Signed ints
+typedef signed char i8;
+typedef signed short i16;
+typedef signed int i32;
+typedef signed long long i64;
+
+// Floats
+typedef float f32;
+typedef double f64;
+
+// Booleans
+typedef int b32;
+typedef char b8;
+
 void test_func(void);
 
 #endif
